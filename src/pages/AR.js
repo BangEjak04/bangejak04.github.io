b2909b2fd3fdbf00f2e51aca1df25b93fdb60a3e
BangEjak04 <rezaandyahw@gmail.com>
2025-10-05 20:31:29 +0700

    0.1.1

--- src/pages/AR.js
+++ src/pages/AR.js
@@ -1,15 +1,23 @@
 export default {
     template: `
         <a-scene>
-          <a-assets>
-              <a-asset-item id="target-file" src="myTarget.zpt"/>
-          </a-assets>
+            <!-- Ask user for camera permissions -->
+            <a-entity zappar-permissions-ui id="permissions"></a-entity>
+            <!-- Browser Compatibility -->
+            <a-entity zappar-compatibility-ui id="compatibility"></a-entity>
 
-          <a-entity camera zappar-camera></a-entity>
+            <a-assets>
+            </a-assets>
 
-          <a-entity zappar-image="#target-file">
-              <!-- YOUR 3D CONTENT GOES HERE -->
-          </a-entity>
-      </a-scene>
+            <a-entity camera zappar-camera></a-entity>
+
+            <a-entity zappar-instant="placement-mode: true" id="instant-group">
+
+                <!-- PLACE 3D OBJECTS HERE TO TRACK FROM THE INSTANT ANCHOR -->
+                <a-box position="0 0.5 0"></a-box>
+
+            </a-entity>
+
+        </a-scene>
     `
 }
